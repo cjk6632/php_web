6905eef35018784dee02abb1cf72361d092bb222
cjk6632 <cjk6632@naver.com>
2020-06-04 19:42:31 +0900

    index.php

--- index.php
+++ index.php
@@ -9,3 +9,10 @@ $con = mysqli_connect('15.164.220.30','root','white!2577','study');
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
+
+
+
+$sql = "SELECT * FROM member";
+$result = mysqli_query($con, $sql);
+var_dump($result->num_rows);
+# var_dump는 화면에 찍어주는 변수(배열 찍을때) / echo를 더 많이 사용
