93bc08c8d68d2f86afc8d3ebe70621196c105aae
cjk6632 <cjk6632@naver.com>
2020-06-04 19:38:10 +0900

    index.php

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-phpinfo();
+# php 변수는 $ 붙여서 사용
+
+
+// Create connection
+$con = mysqli_connect('15.164.220.30','root','white!2577','study');
+
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
