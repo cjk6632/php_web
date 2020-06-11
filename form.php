a88a0b16f9837a889933c688563001b0cd6a3bc1
cjk6632 <cjk6632@naver.com>
2020-06-11 19:37:09 +0900

    form.php

--- form.php
+++ form.php
@@ -2,15 +2,11 @@
 <body>
     <form method="post" action="login_act.php"> <!-- 노출되지 않은 다른 파일에서 생성 -->
         id : <input type="text" name="id" />
-        password : <input type="text" name="pass" />
+        password : <input type="password" name="pass" />
+        <!-- type을 password로 바꾸면 post 시에도 노출 x -->
         <!-- name= 뒤에 들어가는 부분들이 URL 부분에 나오는 파라미터의 이름 -->
         <input type="submit" />
     </form>
 </body>
 </html>
 
-<!-- post로 method 작성 시,
-<?php
-echo $_POST['id'].','.$_POST['password'];
-?>
-의 작성이 필요함 -->
--- login_act.php
+++ login_act.php
@@ -1,3 +1,17 @@
 <?php
     var_dump($_POST)
+
+    // Create connection
+    $con = mysqli_connect('15.164.220.30','root','white!2577','study');
+
+    // Check connection
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
+
+    $sql = "SELECT * FROM member where id = '".$_POST['id']."' and pw = '".$_POST['pass']."'";
+    $result = mysqli_query($con, $sql);
+
+    var_dump($result)
 ?>
