d3c5538b25c2121959c5e57e7df94cadc5a8801f
cjk6632 <cjk6632@naver.com>
2020-06-11 20:09:04 +0900

    form.php

--- login_act.php
+++ login_act.php
@@ -3,6 +3,8 @@
 
     ini_set("display_errors", 1);
 
+    session_start(); # php에서 세션 연결환경 시작
+
     // Create connection
     $con = mysqli_connect('15.164.220.30','root','white!2577','study');
 
@@ -13,8 +15,13 @@
 
 
     $sql = "SELECT * FROM members where id = '".$_POST['id']."' and password = '".$_POST['pass']."'";
-    echo $sql;
     $result = mysqli_query($con, $sql);
+
+    $row = mysqli_fetch_array($result);
+
+    if ($result['num_rows'] > 0) {
+        $_SESSION['id'] = $row[0]['id'];
+    }
 
     var_dump($result)
 ?>
