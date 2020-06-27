a545c6d29baff152960835b0b7f368a33c7ce008
cjk6632 <cjk6632@naver.com>
2020-06-27 15:13:50 +0900

    DB 연동코드 (다른 파일에서 해당 파일 include 하는용)

---
+++ DB.php
@@ -0,0 +1,18 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+session_start();
+header('Content-Type: text/html; charset=utf-8'); // utf-8인코딩
+
+// DB주소, DB계정아이디, DB계정비밀번호, "DB이름"
+$con = new mysqli('xx.xx.xx.xx','xx','xx','study');
+
+mysqli_query($con, "set names utf8");
+
+if(mysqli_connect_errno($con))
+    echo 'DB 연결실패';
+else
+    //echo 'DB 연결성공';
+?>
